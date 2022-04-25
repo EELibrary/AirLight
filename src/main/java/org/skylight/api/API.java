e6d6966a10cc6501985df6cb0d4c6bb6e3020a9c
BuildTools <unconfigured@null.spigotmc.org>
2022-04-25 12:58:46 +0800

    Add API

---
+++ src/main/java/org/skylight/api/API.java
@@ -0,0 +1,39 @@
+package org.skylight.api;
+
+import net.minecraft.server.MinecraftServer;
+import org.bukkit.entity.Minecart;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+public final class API {
+    public static void forceKickPlayer(String name){
+        if(name!=null) {
+            try {
+                EntityPlayerMP entityplayermp = MinecraftServer.getServerInst().getPlayerList().getPlayerByUsername(name);
+                if (entityplayermp != null) {
+                    entityplayermp.connection.getNetworkManager().forceDisconnect();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void doNotHandlePlayerPacket(String name){
+        if(name!=null) {
+            try {
+                EntityPlayerMP entityplayermp = MinecraftServer.getServerInst().getPlayerList().getPlayerByUsername(name);
+                if (entityplayermp != null) {
+                    entityplayermp.connection.getNetworkManager().handlePacket.set(false);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void enablePPSLimitor(int pps){
+        net.minecraft.network.NetworkManager.enablePPSLimitor = true;
+        net.minecraft.network.NetworkManager.ppsl = pps;
+    }
+}
